7679450dba481430bc659352beefe7abad5a3d8b
mklucey97 <mklucey97@gmail.com>
2025-07-26 18:31:19 -0600

    Set header data with page title for Title Generation Form

--- title.php
+++ title.php
@@ -12,4 +12,6 @@ $mustache = new Mustache_Engine;
 $header = file_get_contents('templates/header.html');
 $body   = file_get_contents('templates/title.html');
 $footer = file_get_contents('templates/footer.html');
+
+$header_data = ["pagetitle" => "Title Generation Form"];
 ?>

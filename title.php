c05639b7e96dd4518901e69f6f1de79ddf5eaca0
mklucey97 <mklucey97@gmail.com>
2025-07-27 16:46:14 -0600

    Output rendered header template with dynamic title

--- title.php
+++ title.php
@@ -16,4 +16,6 @@ $footer = file_get_contents('templates/footer.html');
 $header_data = ["pagetitle" => "Title Generation Form"];
 $body_data   = [];
 $footer_data = ["year" => date("Y")];
+
+echo $mustache->render($header, $header_data) . PHP_EOL;
 ?>

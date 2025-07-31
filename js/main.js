2310618dbed7f5a7afe05bbead163e2c5497f9f0
mklucey97 <mklucey97@gmail.com>
2025-07-30 20:02:47 -0600

    Implement client-side form validation and input clearing in main.js for Assignment 5

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * File: main.js
+ * Author: Mary Lucey
+ * Date: 07-31-2025
+ * Description: This JavaScript file provides client-side functionality including
+ *              form validation and clearing input fields for Assignment 5.
+ *              Linked to title.html as part of the Mustache-templated PHP project.
+ * 
+ * Assignment 5: Client Side Validation
+ */
